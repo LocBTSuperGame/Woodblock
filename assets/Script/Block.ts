17fea384160011f6abc4e365148fced92b8e3d11
LocBTSuperGame <locbt@supergame.info>
2025-06-09 17:52:55 +0700

    feat

--- assets/Script/Block.ts
+++ assets/Script/Block.ts
@@ -1,5 +1,4 @@
-// filepath: wood-block-jam/wood-block-jam/assets/Script/Block.ts
-import { _decorator, Component, Vec3, Node } from 'cc';
+import { _decorator, Component, Vec3 } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('Block')
@@ -7,23 +6,7 @@ export class Block extends Component {
     @property(Vec3)
     position: Vec3 = new Vec3(0, 0, 0);
 
-    private isDragging: boolean = false;
-
     start() {
         this.node.setPosition(this.position);
-    }
-
-    onTouchStart() {
-        this.isDragging = true;
-    }
-
-    onTouchMove(touchPosition: Vec3) {
-        if (this.isDragging) {
-            this.node.setPosition(touchPosition);
-        }
-    }
-
-    onTouchEnd() {
-        this.isDragging = false;
     }
 }
